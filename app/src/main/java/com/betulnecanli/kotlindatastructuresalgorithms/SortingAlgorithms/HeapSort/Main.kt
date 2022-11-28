6f39cf610513f03ca0953a0e0a42c642272d893c
betulnecanli <betulnecanli@gmail.com>
2022-11-28 16:44:20 +0300

    HeapSortCompleted

--- app/src/main/java/com/betulnecanli/kotlindatastructuresalgorithms/SortingAlgorithms/HeapSort/Main.kt
+++ app/src/main/java/com/betulnecanli/kotlindatastructuresalgorithms/SortingAlgorithms/HeapSort/Main.kt
@@ -2,6 +2,24 @@ package com.betulnecanli.kotlindatastructuresalgorithms.SortingAlgorithms.HeapSo
 
 fun main(){
     val array = arrayOf(6, 12, 2, 26, 8, 18, 21, 9, 5)
-    array.heapSort()
+    val ascending = Comparator { first: Int, second: Int ->
+        when {
+            first < second -> -1
+            first > second -> 1
+            else -> 0
+        }
+    }
+    array.heapSort(ascending)
     print(array.joinToString())
 }
+
+        //The current example of heap sort sorts the elements in ascending order. How would
+        //you sort in descending order?
+        /*val descending = Comparator { first: Int, second: Int ->
+         when {
+         first < second -> 1
+         first > second -> -1
+         else -> 0
+         }
+        }*/
+
