63352d2237cf113fe0952dc1538dd992e407244a
betulnecanli <betulnecanli@gmail.com>
2022-11-30 17:46:43 +0300

    AVLTreeCompleted

--- app/src/main/java/com/betulnecanli/kotlindatastructuresalgorithms/Trees/TheHeapDataStructure/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.betulnecanli.kotlindatastructuresalgorithms.Trees.TheHeapDataStructure
-
-fun main(){
-    val array = arrayListOf(1, 12, 3, 4, 1, 6, 8, 7) // 1
-   /*val priorityQueue = ComparableHeapImpl.create(array) // 2
-    while (!priorityQueue.isEmpty) { // 3
-        println(priorityQueue.remove())
-    }*/
-}
